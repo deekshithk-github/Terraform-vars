5be6aaf245223fc2f48a7effde82981e626cf3c1
jenkins-examples <81957488+deekshithk-github@users.noreply.github.com>
2024-09-09 18:03:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   features {}
+
+  client_id       = "a9c857b6-fcec-4c65-b2a2-0d51d69b838f"
+  client_secret   = "Vhw8Q~5V_tV4JuDVd2KO6JtG5kIc6Hy4J~7dwcug"
+  tenant_id       = "3ee1eedc-6900-47ee-bbd6-409bccfaa8ea"
+  subscription_id = "52465cd3-77c1-4874-a0e4-f280e0578ad8"
 }
